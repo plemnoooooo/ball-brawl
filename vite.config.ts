6aa87961b17d85a4c64f624a910c59dfeb1e50d9
plemno <plemnotl@gmail.com>
2024-06-03 13:01:27 +0000

    re-enabled secure option

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
         host: "0.0.0.0",
         proxy: {
             "/socket.io/": {
-                secure: false,
+                changeOrigin: true,
                 ws: true,
             }
         }

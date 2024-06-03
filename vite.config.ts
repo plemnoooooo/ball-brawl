b916f5c1596687ef95c197dffd7fb86192562ee6
plemno <plemnotl@gmail.com>
2024-06-03 14:11:32 +0000

    replaced rewrite with resolve

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,7 @@ import socketIoPlugin from "./lib/socketIoPlugin";
 import { runServerEvents } from "./src/utils";
 
 export default defineConfig({
-    server: {
-        host: "0.0.0.0",
-        proxy: {
-            "/socket.io/": {
-                rewrite(path) {
-                    return path;
-                }
-            }
-        }
-    },
+    server: { host: "0.0.0.0" },
+    resolve: { alias: { "socket.io": "socket.io" } },
     plugins: [socketIoPlugin(runServerEvents)]
 });

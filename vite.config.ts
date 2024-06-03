826df4c2864e2c5ae162dc85aaf822db50d1a088
plemno <plemnotl@gmail.com>
2024-06-03 11:13:17 +0000

    configured server host

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import socketIoPlugin from "./lib/socketIoPlugin";
 import { runServerEvents } from "./src/utils";
 
 export default defineConfig({
+    server: { host: "0.0.0.0" },  
     plugins: [socketIoPlugin(runServerEvents)]
 });

8aa7bc5062670ebce4a12c8674f415157ecbd2ed
plemno <plemnotl@gmail.com>
2024-06-03 11:26:13 +0000

    configured for socket.io

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import socketIoPlugin from "./lib/socketIoPlugin";
 import { runServerEvents } from "./src/utils";
 
 export default defineConfig({
-    server: { host: "0.0.0.0" },  
+    server: {
+        host: "0.0.0.0",
+        proxy: {
+            '/socket.io/': {
+                target: 'http://localhost:5002',
+                changeOrigin: true,
+                secure: false,
+                ws: true,
+            },
+        }
+    },  
     plugins: [socketIoPlugin(runServerEvents)]
 });
